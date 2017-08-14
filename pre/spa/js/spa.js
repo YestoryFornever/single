9945f25b2ca51aca05a9599aa0c32fdf68cf1a65
yestoryfornever <guosj90@163.com>
2017-08-14 14:30:13 +0800

    init spa

---
+++ pre/spa/js/spa.js
@@ -0,0 +1,10 @@
+/*
+ * spa.js
+ * Root namespace module
+ */
+var spa = (function(){
+	var initModule = function($container){
+		spa.shell.initModule( $container );
+	};
+	return {initModule:initModule};
+}());
