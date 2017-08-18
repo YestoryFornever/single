7177be6f273edd0516fd60701e35906b3d19b3df
yestoryfornever <guosj90@163.com>
2017-08-18 16:05:50 +0800

    updateavatar

--- pre/spa/js/spa.fake.js
+++ pre/spa/js/spa.fake.js
@@ -47,25 +47,93 @@ spa.fake = (function(){
 	};
 
 	mockSio = (function(){
-		var on_sio, emit_sio, callback_map={};
+		var on_sio, emit_sio, emit_mock_msg,
+			send_listchange,
+			listchange_idto,
+			callback_map={},
+			peopleList = getPeopleList();
 
 		on_sio = function( msg_type, callback ){
 			callback_map[ msg_type ] = callback;
 		};
 
 		emit_sio = function( msg_type, data ){
+			var person_map;
 			if( msg_type === 'adduser' && callback_map.userupdate ){
 				setTimeout( function(){
-					callback_map.userupdate(
-						[{
-							_id: makeFakeId(),
-							name: data.name,
-							css_map: data.css_map
-						}]
-					);
+					person_map = {
+						_id:makeFakeId(),
+						name:data.name,
+						css_map:data.css_map
+					};
+					peopleList.push( person_map );
+					callback_map.userupdate([person_map]);
 				},3000);
 			}
+			if( msg_type === 'updatechat' && callback_map.updatechat ){
+				setTimeout(function(){
+					var user = spa.model.people.get_user();
+					callback_map.updatechat([{
+						dest_id:user.id,
+						dest_name: user.name,
+						sender_id: data.dest_id,
+						msg_text: 'Thanks for the note, ' + user.name
+					}]);
+				},2000);
+			}
+			if(msg_type === 'leavechat'){
+				delete callback_map.listchange;
+				delete callback_map.updatechat;
+
+				if(listchange_idto){
+					clearTimeout( listchange_idto );
+					listchange_idto = undefined;
+				}
+				send_listchange();
+			}
+
+			if( msg_type === 'updateavatar' && callback_map.listchange ){
+				var i;
+				for( i=0; i<peopleList.length; i++ ){
+					if( peopleList[i]._id === data.person_id ){
+						peopleList[i].css_map = data.css_map;
+						break;
+					}
+				}
+				callback_map.listchange([peopleList]);
+			}
 		};
+
+		emit_mock_msg = function(){
+			setTimeout( function(){
+				var user = spa.model.people.get_user();
+				if( callback_map.updatechat ){
+					callback_map.updatechat([{
+						dest_id: user.id,
+						dest_name: user.name,
+						sender_id: 'id_04',
+						msg_text: 'Hi there ' + user.name + '!wilma here.'
+					}]);
+				}else{
+					emit_mock_msg();
+				}
+			},8000);
+		}
+
+		send_listchange = function(){
+			listchange_idto = setTimeout(function(){
+				if(callback_map.listchange ){
+					callback_map.listchange([peopleList]);
+					emit_mock_msg();
+					listchange_idto = undefined;
+				}else{
+					send_listchange();
+				}
+			},1000);
+		};
+
+		send_listchange();
+
 		return { emit: emit_sio, on: on_sio };
 	})();
 	return { 
--- pre/spa/js/spa.model.js
+++ pre/spa/js/spa.model.js
@@ -75,6 +75,7 @@ spa.model = (function(){
 		stateMap.user.id = user_map._id;
 		stateMap.user.css_map = user_map.css_map;
 		stateMap.people_cid_map[user_map._id] = stateMap.user;
+		chat.join();
 
 		$.gevent.publish( 'spa-login', [stateMap.user]);
 	}
@@ -143,6 +144,7 @@ spa.model = (function(){
 		logout = function(){
 			var is_removed, user = stateMap.user;
 
+			chat._leave();
 			is_removed = removePerson( user );
 			stateMap.user = stateMap.anon_user;
 
@@ -164,13 +166,21 @@ spa.model = (function(){
 
 	chat = (function(){
 		var _publish_listchange,
+			_publish_updatechat,
 			_update_list,
 			_leave_chat,
-			join_chat;
+
+			get_chatee,
+			join_chat,
+			send_msg,
+			set_chatee,
+			update_avatar,
+			chatee = null;
 
 		_update_list = function( arg_list ){
 			var i, person_map, make_person_map,
-				people_list = arg_list[0];
+				people_list = arg_list[0],
+				is_chatee_online = false;
 
 			clearPeopleDb();
 
@@ -188,9 +198,15 @@ spa.model = (function(){
 					id: person_map._id,
 					name: person_map.name
 				};
+				if( chatee && chatee.id === make_person_map.id ){
+					is_chatee_online = true;
+				}
 				makePerson( make_person_map );
 			}
 			stateMap.people_db.sort('name');
+			if(chatee && !is_chatee_online ){
+				set_chatee('');
+			}
 		};
 
 		_publish_listchange = function(arg_list){
@@ -198,11 +214,23 @@ spa.model = (function(){
 			$.gevent.publish( 'spa-listchange',[arg_list]);
 		};
 
+		_publish_updatechat = function(arg_list){
+			var msg_map = arg_list[0];
+			if(!chatee){set_chatee(msg_map.sender_id);}
+			else if( msg_map.sender_id !== stateMap.user.id
+				&& msg_map.sender_id !== chatee.id
+			){ set_chatee( msg_map.sender_id); }
+			$.gevent.publish('spa-updatechat',[msg_map]);
+		};
+
 		_leave_chat = function(){
 			var sio = isFakeData ? spa.fake.mockSio :spa.data.getSio();
+			chatee = null;
 			stateMap.is_connected = false;
 			if(sio){sio.emit('leave_chat');}
 		};
+
+		get_chatee = function(){ return chatee;};
 
 		join_chat = function(){
 			var sio;
@@ -213,12 +241,61 @@ spa.model = (function(){
 			}
 			sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
 			sio.on('listchange',_publish_listchange);
+			sio.on('updatechat',_publish_updatechat );
 			stateMap.is_connected = true;
 			return true;
 		}
+
+		send_msg = function( msg_text ){
+			var msg_map,
+				sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+			if(!sio){return false;}
+			if(!(stateMap.user&&chatee)){return false;};
+
+			msg_map = {
+				dest_id:chatee.id,
+				dest_name:chatee.name,
+				sender_id:stateMap.user.id,
+				msg_text:msg_text
+			};
+
+			_publish_updatechat([msg_map]);
+			sio.emit('updatechat',msg_map);
+			return true;
+		};
+
+		set_chatee = function(person_id){
+			var new_chatee;
+			new_chatee = stateMap.people_cid_map[person_id];
+			if(new_chatee){
+				if(chatee && chatee.id === new_chatee.id){
+					return false;
+				}
+			}else{
+				new_chatee = null;
+			}
+			$.gevent.publish('spa-setchatee',
+				{old_chatee:chatee,new_chatee:new_chatee}
+			);
+			chatee = new_chatee;
+			return true;
+		}
+
+		update_avatar = function( avatar_update_map ){
+			var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+			if( sio ){
+				sio.emit( 'updateavatar',avatar_update_map );
+			}
+		}
+
 		return {
 			_leave:_leave_chat,
-			join:join_chat
+			get_chatee: get_chatee,
+			join:join_chat,
+			send_msg: send_msg,
+			set_chatee: set_chatee,
+			update_avatar: update_avatar
 		}
 	})();
 
