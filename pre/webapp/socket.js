74e280528ea501cf270b8d3ad03b13e6720d2935
YestoryFornever <guosj90@163.com>
2017-08-20 01:19:18 +0800

    fs.watchFile 热更新

--- pre/webapp/socket.js
+++ pre/webapp/socket.js
@@ -1,20 +1,40 @@
-let countUp,
+let setWatch,
 	http = require('http'),
 	express = require('express'),
 	socketIo = require('socket.io'),
+	fs = require('fs'),
 
 	app = express(),
 	server = http.createServer(app),
 	io = socketIo.listen(server),
-	countIdx = 0;
+	watchMap = {};
 
-countUp = function(){
-	countIdx++;
-	console.log(countIdx);
-	io.sockets.send(countIdx);
+setWatch = function( url_path, file_type ){
+	url_path = url_path.split('?')[0];//默认带有？后缀
+	console.log( url_path );
+	if(! watchMap[url_path]){
+		console.log('setting watch on ' + "sockets/"+url_path.slice(1));
+		fs.watchFile(
+			"sockets/"+url_path.slice(1),
+			function(current,previous){
+				if( current.mtime !== previous.mtime ){
+					console.log('file changed',url_path);
+					io.sockets.emit(file_type,url_path);
+				}
+			}
+		);
+		watchMap[url_path] = true;
+	}
 }
-
-app.use( express.static(__dirname+'/public') );
+app.use( (req,res,next)=>{
+	if( req.url.indexOf('/js/')>=0){
+		setWatch(req.url,'script');
+	}else if(req.url.indexOf('/css/')>=0){
+		setWatch(req.url,'stylesheet');
+	}
+	next();
+});
+app.use( express.static(__dirname+'/sockets') );
 app.use( 
 	express.Router().get('/', (req, res, next)=>{
 		res.redirect('./socket.html');
@@ -22,5 +42,3 @@ app.use(
 );
 server.listen(9999);
 console.log(9999);
-
-setInterval(countUp,1000);
