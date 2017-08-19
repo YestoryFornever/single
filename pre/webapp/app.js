3a1c6ecc900967c4bc81d687be5b04a80051c857
YestoryFornever <guosj90@163.com>
2017-08-19 19:41:05 +0800

    用户curd

--- pre/webapp/app.js
+++ pre/webapp/app.js
@@ -8,15 +8,35 @@ let http = require('http'),
 	server = http.createServer( app );
 
 let router = express.Router();
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next)=>{
 	res.redirect('./spa.html');
+});
+router.get('/user/list',(req,res,next)=>{
+	res.contentType('json');
+	res.send({title:'user list'});
+});
+router.post('/user/create',(req,res,next)=>{
+	res.contentType('json');
+	res.send({title:'user create'});
+});
+router.get('/user/read/:id([0-9]+)',(req,res,next)=>{
+	res.contentType('json');
+	res.send({title:'user with id' + req.params.id + ' found'});
+});
+router.post('/user/update/:id([0-9]+)',(req,res,next)=>{
+	res.contentType('json');
+	res.send({title:'user with id' + req.params.id + ' updated'});
+});
+router.get('/user/delete/:id([0-9]+)',(req,res,next)=>{
+	res.contentType('json');
+	res.send({title:'user with id' + req.params.id + ' deleted'});
 });
 
 app.use( bodyParser.json() );
 app.use( methodOverride('X-HTTP-Method-Override') );
 app.use( express.static(__dirname+'/public') );
 
-app.use('/',router);
+app.use('/',router);// '/'可省略，挂载功能默认就是使用slash
 
 if (app.get('env') === 'development') {
 	app.use( morgan('combined') );
