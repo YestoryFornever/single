c845e3d5ba81f28279341c4676cefc6cf8122fae
YestoryFornever <guosj90@163.com>
2017-08-19 19:48:34 +0800

    obj_type

--- pre/webapp/app.js
+++ pre/webapp/app.js
@@ -7,34 +7,33 @@ let http = require('http'),
 	app = express(),
 	server = http.createServer( app );
 
-let router = express.Router();
-router.get('/', (req, res, next)=>{
-	res.redirect('./spa.html');
-});
-router.all('/user/*?',(req,res,next)=>{
-	res.contentType('json');
-	next();
-});
-router.get('/user/list',(req,res,next)=>{
-	res.send({title:'user list'});
-});
-router.post('/user/create',(req,res,next)=>{
-	res.send({title:'user create'});
-});
-router.get('/user/read/:id([0-9]+)',(req,res,next)=>{
-	res.send({title:'user with id ' + req.params.id + ' found'});
-});
-router.post('/user/update/:id([0-9]+)',(req,res,next)=>{
-	res.send({title:'user with id ' + req.params.id + ' updated'});
-});
-router.get('/user/delete/:id([0-9]+)',(req,res,next)=>{
-	res.send({title:'user with id ' + req.params.id + ' deleted'});
-});
-
 app.use( bodyParser.json() );
 app.use( methodOverride('X-HTTP-Method-Override') );
 app.use( express.static(__dirname+'/public') );
 
+let router = express.Router();
+router.get('/', (req, res, next)=>{
+	res.redirect('./spa.html');
+});
+router.all('/:obj_type/*?',(req,res,next)=>{
+	res.contentType('json');
+	next();
+});
+router.get('/:obj_type/list',(req,res,next)=>{
+	res.send({title:req.params.obj_type + ' list'});
+});
+router.post('/:obj_type/create',(req,res,next)=>{
+	res.send({title:req.params.obj_type + ' create'});
+});
+router.get('/:obj_type/read/:id([0-9]+)',(req,res,next)=>{
+	res.send({title:req.params.obj_type + ' with id ' + req.params.id + ' found'});
+});
+router.post('/:obj_type/update/:id([0-9]+)',(req,res,next)=>{
+	res.send({title:req.params.obj_type + ' with id ' + req.params.id + ' updated'});
+});
+router.get('/:obj_type/delete/:id([0-9]+)',(req,res,next)=>{
+	res.send({title:req.params.obj_type + ' with id ' + req.params.id + ' deleted'});
+});
 app.use('/',router);// '/'可省略，挂载功能默认就是使用slash
 
 if (app.get('env') === 'development') {
