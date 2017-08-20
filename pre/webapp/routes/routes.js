686ca19942cfaeba0db57652a517e02738f98040
YestoryFornever <guosj90@163.com>
2017-08-20 13:59:02 +0800

    connected to mongoldb

--- pre/webapp/routes/routes.js
+++ pre/webapp/routes/routes.js
@@ -1,5 +1,18 @@
 let express = require('express'),
 	router = express.Router();
+
+let mongodb = require('mongodb'),
+	mongoServer = new mongodb.Server(
+		'localhost',
+		27017
+		// mongodb.Connection.DEFAULT_PORT
+	),
+	dbHandle = new mongodb.Db(
+		'spa',
+		mongoServer,
+		{safe:true}
+	);
+dbHandle.open(function(){console.log('** Connected to MongoDB **')});
 	
 router.get('/', (req, res, next)=>{
 	res.redirect('./spa.html');
