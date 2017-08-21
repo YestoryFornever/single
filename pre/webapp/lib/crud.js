64ca41aad469cfc686f2c12d634802f1ff242c95
yestoryfornever <guosj90@163.com>
2017-08-21 14:42:06 +0800

    curd module

---
+++ pre/webapp/lib/crud.js
@@ -0,0 +1,194 @@
+let checkType,
+    constructObj,
+    readObj,
+    updateObj,
+    destoryObj,
+    loadSchema,
+    checkSchema,
+    clearIsOnline,
+    objTypeMap = {'user':{}};
+
+let JSV = require('JSV').JSV,
+    validator = JSV.createEnvironment();
+    
+let mongodb = require('mongodb'),
+    mongoServer = new mongodb.Server(
+        'localhost',
+        '27017'
+    ),
+    dbHandle = new mongodb.Db(
+		'spa',
+		mongoServer,
+		{safe:true}
+	),
+    makeMongoId = mongodb.ObjectID;
+
+let fs = require('fs');
+
+loadSchema = function( schema_name, schema_path ){
+	fs.readFile( schema_path,'utf-8',(err,data)=>{
+		objTypeMap[ schema_name ] = JSON.parse( data );
+	});
+};
+
+checkSchema = function( obj_type, obj_map, callback ){
+	var schema_map = objTypeMap[ obj_type ],
+		report_map = validator.validate( obj_map, schema_map );
+	// console.log(report_map);
+	callback( report_map.errors );
+};
+
+clearIsOnline = function(){
+    updateObj(
+        'user',
+        {is_online:true},
+        {is_online:false},
+        (response_map)=>{
+            console.log('All users set to offline', response_map);
+        }
+    );
+};
+
+checkType = function(obj_type){
+    if(!objTypeMap[obj_type]){
+        return ({
+            erro_msg: 'Object type "' + obj_type
+                + '" is not supported.'
+        });
+    }
+    return null;
+};
+
+constructObj = function(obj_type, obj_map, callback){
+    let type_check_map = checkType( obj_type );
+    if( type_check_map ){
+        callback( type_check_map );
+    }
+    checkSchema(
+        obj_type,
+        obj_map,
+        (error_list)=>{
+            if(error_list.length === 0){
+                dbHandle.collection(
+                    obj_type,
+                    (outer_error,collection)=>{
+                        let options_map = {safe:true};
+                        collection.insert(
+                            obj_map,
+                            options_map,
+                            (inner_error,result_map)=>{
+                                callback(result_map);
+                            }
+                        );
+                    }
+                );
+            }else{
+                callback({
+                    error_msg:'Input document not valid',
+                    error_list:error_list
+                });
+            }
+        }
+    );
+};
+readObj = function(obj_type, find_map, fields_map, callback){
+    let type_check_map = checkType( obj_type );
+    if( type_check_map ){
+        callback( type_check_map );
+        return;
+    }
+    dbHandle.collection(
+        obj_type,
+        (outer_error,collection)=>{
+            collection.find( find_map, fields_map ).toArray(
+                (inner_error, map_list)=>{
+                    callback( map_list );
+                }
+            );
+        }
+    );
+};
+updateObj = function(obj_type, find_map, set_map, callback){
+    let type_check_map = checkType(obj_type);
+    if(type_check_map){
+        callback(type_check_map);
+        return;
+    }
+    checkSchema(
+		obj_type, set_map,
+		function(error_list){
+			if( error_list.length === 0 ){
+				dbHandle.collection(
+					obj_type,
+					(outer_error,collection)=>{
+                        collection.update(
+                            find_map,
+                            {
+                                $set:set_map
+                            },
+                            {
+                                safe:true,
+                                multi:true,
+                                upsert:false
+                            },
+                            (inner_error,update_count)=>{
+                                callback({ update_count:update_count });
+                            }
+                        );
+					}
+				);
+			}else{
+				callback({
+					error_msg:'Input document not valid',
+					error_list:error_list
+				});
+			}
+		}
+	);
+};
+destoryObj = function(obj_type, find_map, callback){
+    let type_check_map = checkType( obj_type );
+    if( type_check_map ){
+        callback( type_check_map );
+        return;
+    }
+    dbHandle.collection(
+        obj_type,
+		(outer_error,collection)=>{
+			let options_map = {safe:true,single:true};
+			collectin.remove(
+				find_map,
+				options_map,
+				(inner_error,delete_count)=>{
+					callback({ delete_count: delete_count })
+				}
+			);
+		}
+    );
+};
+
+console.log('** CRUD module loaded **');
+
+dbHandle.open(function(){
+    console.log('** Connected to MongoDB **');
+    clearIsOnline();
+});
+
+(function(){
+	var schema_name, schema_path;
+	for(schema_name in objTypeMap){
+		if( objTypeMap.hasOwnProperty( schema_name ) ){
+			schema_path = __dirname + '/' + schema_name + '.json';
+			loadSchema( schema_name, schema_path );
+		}
+	}
+})();
+
+module.exports = {
+    makeMongoId:mongodb.ObjectID,
+    checkType:checkType,
+    construct:constructObj,
+    read:readObj,
+    update:updateObj,
+    destory:destoryObj
+};
--- pre/webapp/lib/routes.js
+++ pre/webapp/lib/routes.js
@@ -1,171 +1,60 @@
 let express = require('express'),
 	router = express.Router(),
-	loadSchema,
-	fs = require('fs'),
-	checkSchema,
-	JSV = require('JSV').JSV;
-
-let mongodb = require('mongodb'),
-	mongoServer = new mongodb.Server(
-		'localhost',
-		27017
-		// mongodb.Connection.DEFAULT_PORT
-	),
-	dbHandle = new mongodb.Db(
-		'spa',
-		mongoServer,
-		{safe:true}
-	),
-	validator = JSV.createEnvironment(),
-	makeMongoId = mongodb.ObjectID,
-	objTypeMap = {'user':{}};
-
-dbHandle.open(function(){
-	console.log('** Connected to MongoDB **');
-});
-	
-loadSchema = function( schema_name, schema_path ){
-	fs.readFile( schema_path,'utf-8',(err,data)=>{
-		objTypeMap[ schema_name ] = JSON.parse( data );
-	});
-};
-
-checkSchema = function( obj_type, obj_map, callback ){
-	var schema_map = objTypeMap[ obj_type ],
-		report_map = validator.validate( obj_map, schema_map );
-	// console.log(report_map);
-	callback( report_map );
-};
-
-(function(){
-	var schema_name, schema_path;
-	for(schema_name in objTypeMap){
-		if( objTypeMap.hasOwnProperty( schema_name ) ){
-			schema_path = __dirname + '/' + schema_name + '.json';
-			loadSchema( schema_name, schema_path );
-		}
-	}
-})();
+	crud = require('./crud');
 
 router.get('/', (req, res, next)=>{
 	res.redirect('./spa.html');
 });
 router.all('/api/:obj_type/*?',(req,res,next)=>{
 	res.contentType('json');
-	if( objTypeMap[req.params.obj_type]){
-		next();
-	}else{
-		res.send({error_msg:req.params.obj_type+'is not a valid object type'});
-	}
+	next();
 });
 router.get('/api/:obj_type/list',(req,res,next)=>{
-	dbHandle.collection(
+	crud.read(
 		req.params.obj_type,
-		(outer_error,collection)=>{
-			collection.find().toArray(
-				(inner_error,map_list)=>{
-					res.send(map_list);
-				}
-			);
+		{},
+		{},
+		(map_list)=>{
+			res.send(map_list);
 		}
 	);
 });
 router.post('/api/:obj_type/create',(req,res,next)=>{
-	var obj_type = req.params.obj_type,
-		obj_map = req.body;
-	checkSchema(
-		obj_type, obj_map,
-		function(error_list){
-			if( error_list.errors.length === 0 ){
-				dbHandle.collection(
-					obj_type,
-					function(outer_error,collection){
-						var options_map = { safe: true };
-						collection.insert(
-							obj_map,
-							options_map,
-							function(inner_error,result_map){
-								res.send(result_map);
-							}
-						);
-					}
-				);
-			}else{
-				res.send({
-					error_msg: 'Input document not valid',
-					error_list: error_list.errors
-				});
-			}
+	crud.construct(
+		req.params.obj_type,
+		{_id:makeMongoId(req.params.id)},
+		{},
+		(map_list)=>{
+			res.send(map_list);
 		}
 	);
 });
 router.get('/api/:obj_type/read/:id([0-9]+)',(req,res,next)=>{
-	// res.send({title:req.params.obj_type + ' with id ' + req.params.id + ' found'});
-	let find_map = { _id: makeMongoId( req.params.id ) };
-	dbHandle.collection(
+	crud.read(
 		req.params.obj_type,
-		(outer_error,collection)=>{
-			collection.findOne(
-				find_map,
-				(inner_error,result_map)=>{
-					res.send(result_map);
-				}
-			);
+		{_id: makeMongoId(req.params.id)},
+		{},
+		(map_list)=>{
+			res.send(map_list);
 		}
 	);
 });
 router.post('/api/:obj_type/update/:id([0-9]+)',(req,res,next)=>{
-	// res.send({title:req.params.obj_type + ' with id ' + req.params.id + ' updated'});
-	let find_map = { _id: makeMongoId( req.params.id )},
-		obj_map = req.body,
-		obj_type = req.params.obj_type;
-	checkSchema(
-		obj_type, obj_map,
-		function(error_list){
-			if( error_list.errors.length === 0 ){
-				dbHandle.collection(
-					obj_type,
-					(outer_error,collection)=>{
-						let sort_order = [],
-							options_map = {
-								'new': true,
-								upsert: false,
-								safe: true
-							};
-						collection.findAndModify(
-							find_map,
-							sort_order,
-							obj_map,
-							options_map,
-							(inner_error,updated_map)=>{
-								res.send(updated_map);
-							}
-						);
-					}
-				);
-			}else{
-				res.send({
-					error_msg:'Input document not valid',
-					error_list:error_list.errors
-				});
-			}
+	crud.update(
+		req.params.obj_type,
+		{_id: makeMongoId(req.params.id)},
+		req.body,
+		(result_map)=>{
+			res.send(result_map);
 		}
 	);
 });
 router.get('/api/:obj_type/delete/:id([0-9]+)',(req,res,next)=>{
-	// res.send({title:req.params.obj_type + ' with id ' + req.params.id + ' deleted'});
-	let find_map = { _id: makeMongoId( req.params.id ) };
-	dbHandle.collection(
+	crud.destory(
 		req.params.obj_type,
-		(outer_error,collection)=>{
-			let options_map = {safe:true,single:true};
-			collectin.remove(
-				find_map,
-				options_map,
-				(inner_error,delete_count)=>{
-					res.send({ delete_count: delete_count })
-				}
-			);
+		{_id: makeMongoId(req.params.id)},
+		(result_map)=>{
+			res.send(result_map);
 		}
 	);
 });
